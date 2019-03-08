a23b6c8e27a43d686388e6720cd4f9748e0149a4
yogesh <yogi.bombe@gmail.com>
2019-03-08 23:44:01 +0530

    init.

--- sessionTwoAssignment2.py
+++ sessionTwoAssignment2.py
@@ -10,5 +10,18 @@ y = [
     [4, 5, 9, 1]
 ]
 
-# there are three for loop
+# result is 3x4
+result = [[0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]]
 
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+            result[i][j] += x[i][k] * y[k][j]
+            
+# result is 3x4
+result = [[sum(a * b for a, b in zip(X_row, Y_col)) for Y_col in zip(*y)] for X_row in x]
+
+for r in result:
+    print(r)
